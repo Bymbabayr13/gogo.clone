e06af76b5eacb452561830091ee9c3acdd28c3ef
Bymbabayr13 <nnorn0210@gmail.com>
2024-01-04 13:12:59 +0800

    asdad

--- javascript/project/lodo.js
+++ javascript/project/lodo.js
@@ -1,4 +1,5 @@
-let task = []
+
+let arr = []
 let Generate = "";
 let toolur = 0;
 const commit = document.getElementById("commit");
@@ -6,70 +7,49 @@ const input = document.getElementById("input");
 const result = document.getElementById("garalt");
 const desc = document.getElementById("description");
 const tailbar = document.getElementById("tailbar");
+
 const Status = document.getElementById("Status");
 const ShowStatus = document.getElementById("ShowStatus");
 const priority = document.getElementById("TaskPriority").value;
 const click = document.getElementById("modal");
 
-
 const todoTasksElement = document.getElementById("TodoTasks");
 const inprogressTasksElement = document.getElementById("InprogressTasks");
 const stuckTasksElement = document.getElementById("StuckTasks");
 const doneTasksElement = document.getElementById("DoneTasks");
+let currentTarget = "";
 const clear = () => {
   input.value = "";
   tailbar.value = "";
-  let currentTarget = "";
+
 };
 const removeTask = (taskId) => {
   document.getElementById(taskId).remove();
+  alert("asdfasf");
 };
-function addTask() {
-  task = [{
-    id:toolur++,
-    title:input.value,
-    tailbar:tailbar.value,
-    priority:ShowStatus.value
-  }]
-
-  render()
-  clear();
-  none();
-  console.log(display)
-}
 
 
-
-
-
-
-
-
-function render(){
- 
-  for(let i = 0 ; i <task.length; i++){
-    let tasks = task[i]
-    if (task.input == "" && task.tailbar == "") {
-      click.style.display = "flex";
-      alert("utga oruul");
-    } else {
-      const Generate = `
-        <div  class="box-1-1" id="task-${tasks.id}">
+function render (){
+  const display = document.getElementById(currentTarget)
+for(let i=0; i<arr.length; i++){
+  let result= ""
+  let arrs = arr[i]
+  const Generate = `
+  <div  class="box-1-1" id="task-${arrs.id}">
         <div class="flex" style="padding-left: 10px">
           <button class="right">
             <img src="./done_FILL0_wght400_GRAD0_opsz24.png" alt="" />
           </button>
-          <div id="garalt" style="margin-bottom:5px">${tasks.title}</div>
-  
+          <div id="garalt" style="margin-bottom:5px">${arrs.title}</div>
           <div   class="end" style="margin-left: auto">
-            <button class="right" onclick="removeTask('task-${tasks.id}')">
+            <button class="right" onclick="removeTask('task-${arr.id}')">
               <img src="./close_FILL0_wght400_GRAD0_opsz24.png" alt="" />
             </button>
           </div>
         
         </div>
         <div class="flex" style="padding-left: 10px">
-          <div id="description">${tasks.tailbar}</div>
+          <div id="description">${arrs.tailbar}</div>
           
           <div class="end" style="margin-left: auto">
             <button class="right">
@@ -83,13 +63,32 @@ function render(){
           </div>
         </div>
         
-          <div  class="Status";> ${tasks.priority}</div>
+          <div  class="Status";> ${arrs.priority}</div>
         
         <div style="padding-left: 10px"></div>
       </div>`;
-  }
+    result +=Generate
+  
+  
 }
 
+display.innerHTML += result
+}
+function addTask() {
+    arr.push = [{
+      id:toolur++,
+      title:input,
+      tailbar:tailbar,
+      priority:ShowStatus
+    }]
+    console.log(result)
+
+    // click.style.display = "flex";
+    render(); 
+    clear();
+    none();
+   
+  
 
   console.log();
 }
@@ -100,3 +99,8 @@ function showModal(TargetId) {
 function none() {
   click.style.display = "none";
 }
+console.log(Generate)
+render();
+
+
+
